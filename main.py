fc02c89f689c6ca7d341bb642a449646d81680c9
VilMe <vilma.medrano00@gmail.com>
2025-09-13 18:41:03 -0500

    add logic for when guess is incorrect and when the guess is correct and user wins game

--- main.py
+++ main.py
@@ -22,5 +22,14 @@ def run_game():
                 print('_', end='')
                 blanks+=1
     
-    print() #adds a blank line
-            
+        print() # adds a blank line
+
+
+        if blanks == 0:
+            print('You got it!')
+            break
+        
+        guess: str = input('Enter a letter: ')
+
+        if guess in guessed:
+            print(f'You already used: "{guess}". Please try another letter!')
